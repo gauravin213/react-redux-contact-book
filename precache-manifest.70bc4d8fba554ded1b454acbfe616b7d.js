4d05a672c638c11c1a835bb345c5aad6d001d450
gaurav <gauravin213@gmail.com>
2020-06-20 22:36:41 +0530

    Updates

---
+++ precache-manifest.70bc4d8fba554ded1b454acbfe616b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0d8302960b98cd397f5099ca94d111",
+    "url": "/react-redux-contact-book/index.html"
+  },
+  {
+    "revision": "4bdbb0507e9c8b3cf434",
+    "url": "/react-redux-contact-book/static/css/main.27f9dbee.chunk.css"
+  },
+  {
+    "revision": "9031bf365e4a14ca3f45",
+    "url": "/react-redux-contact-book/static/js/2.85e35da6.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/react-redux-contact-book/static/js/2.85e35da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bdbb0507e9c8b3cf434",
+    "url": "/react-redux-contact-book/static/js/main.98005fa0.chunk.js"
+  },
+  {
+    "revision": "861a9bb015bf30b658e8",
+    "url": "/react-redux-contact-book/static/js/runtime-main.a2b5d4bb.js"
+  }
+]);
